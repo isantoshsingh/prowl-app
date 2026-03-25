252af4fc3e3fae16b93a0e883c5d18456dd38566
Santosh Singh <singh.santosh786@gmail.com>
2026-03-25 23:29:43 +0530

    admin section changes

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_08_113938) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
     t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at", null: false
+    t.datetime "remember_created_at"
+    t.string "unlock_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
